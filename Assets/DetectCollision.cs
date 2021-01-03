6593e1c19eb8c0f04112da2f6ef4ff9ad43c1f2f
RyanFin <ryanfinlayson125@hotmail.com>
2021-01-03 15:33:46 +0000

    Add level2 and transitions from level1 -> level2

--- Assets/DetectCollision.cs
+++ Assets/DetectCollision.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class DetectCollision : MonoBehaviour
 {
-    private int score;
+    private int score, numCoinsCollectedPerLevel;
     // Start is called before the first frame update
     void Start(){
-
+      numCoinsCollectedPerLevel = 0;
     }
 
     // Update is called once per frame
@@ -21,6 +21,10 @@ public class DetectCollision : MonoBehaviour
       if(tagName == "pick_me"){
         Destroy(col.collider.gameObject);
         score++;
+        numCoinsCollectedPerLevel++;
+        if(SceneManager.GetActiveScene().name == "level1" && numCoinsCollectedPerLevel >= 5 ){
+          SceneManager.LoadScene("level2");
+        }
         print("Score: " + score );
       }
       if(tagName == "avoid_me"){
